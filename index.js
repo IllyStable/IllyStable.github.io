db2c8fd7f2033ba7b9c8f3c9fbfa838b4e864dab
Illy <illystable.champs@gmail.com>
2023-06-07 18:29:29 +0100

    Added background image

---
+++ index.js
@@ -0,0 +1,22 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example
+};
+
+const game = new Phaser.Game(config);
+
+class Example extends Phaser.Scene {
+    constructor() {
+        super();
+    }
+
+    preload() {
+        this.load.image("background", "background.png");
+    }
+    
+    create() {
+        this.add.image(800, 600, 'background');
+    }
+}
